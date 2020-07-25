30442aa1ae8a395186a60bf68490a9453f0c2c53
anil_sikhram <anil.sikhram@maryland.gov>
2020-07-25 17:45:39 -0400

    test controller

--- src/main/java/com/sikhram/smartVillage/SmartVillageApplication.java
+++ src/main/java/com/sikhram/smartVillage/SmartVillageApplication.java
@@ -2,12 +2,20 @@ package com.sikhram.smartVillage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SmartVillageApplication {
+public class SmartVillageApplication extends SpringBootServletInitializer {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartVillageApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SmartVillageApplication.class);
 	}
 
 }
