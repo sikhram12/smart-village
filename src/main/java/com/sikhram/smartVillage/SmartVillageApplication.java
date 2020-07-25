b36f09e362514995384acb62f5b999b08a9cbab5
anil_sikhram <anil.sikhram@maryland.gov>
2020-07-25 16:39:09 -0400

    initial commit

---
+++ src/main/java/com/sikhram/smartVillage/SmartVillageApplication.java
@@ -0,0 +1,13 @@
+package com.sikhram.smartVillage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartVillageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartVillageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sikhram/smartVillage/SmartVillageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sikhram.smartVillage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartVillageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
